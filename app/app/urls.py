4569d72b956f2fdc3e63c481e3f9eb7a63aaf173
saguirregaray1 <saguirregaray1@correo.um.edu.uy>
2024-05-27 13:24:28 -0300

    nc

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/schema", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/schema/docs",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
+    ),
+    path("api/user/", include("user.urls")),
+    path("api/recipe/", include("recipe.urls")),
 ]
